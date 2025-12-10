dcecfe36b7203b99cc61e8522ad76981491315c1
efecini <efecini@gmail.com>
2025-12-10 04:50:11 +0300

    first chapter

---
+++ Udemy:Modern JavaScript From The Beginning 2.0/1.js
@@ -0,0 +1,21 @@
+/*
+1. What is JS?
+- Not related to Java.
+- It is the core tech of the web.
+- It is the high level, interpreted programming language that can be used on the client side as well as on the server side with Node.js
+
+2. What is JS used for?
+- DOM Manipulation(Change content)
+- Event Handling(Respond to user clicks)
+- Asynch Requests (Interact with BE server)
+- Animations & Effects (animate.css & manipulate.css)
+- Date Manipulation (sort, map, filter)
+- SPA (update content without refreshing the page)
+- Creating APIs and web servers (node.js & express)
+
+3. Why Should you learn it?
+Popularity, versatility, easy to learn, community.
+
+4. In settings->prettier, you can change single quote, semi-colon, format on save options.
+
+*/
