4f9d46561f02c637d06af029b6f65490aa73b3a4
efecini <efecini@gmail.com>
2025-12-14 23:19:46 +0300

    objects

--- Udemy:Modern JavaScript From The Beginning 2.0/3-Arrays&Objects.js
+++ Udemy:Modern JavaScript From The Beginning 2.0/3-Arrays&Objects.js
@@ -70,3 +70,124 @@ console.log(arr7.flat());
 console.log(Array.isArray(arr7));
 
 console.log(Array.from('123456'));
+
+// Objects
+
+// Object Literal
+
+const person = {
+  name: 'John Doe',
+  age: 30,
+  isAdmin: true,
+  address: {
+    street: '123 Main st',
+    city: 'Boston',
+    state: 'MA',
+  },
+  hobbies: ['music', 'sports'],
+};
+
+x = person.name;
+console.log(x);
+x = person['age'];
+console.log(x);
+x = person.address.state;
+console.log(x);
+
+x = person;
+console.log(x);
+delete person.age;
+person.hasChildren = true;
+console.log(x);
+
+person.greet = function () {
+  console.log('Hello, my name is ${this.name}');
+};
+console.log(x);
+
+const todo = new Object();
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+const obj3 = { ...obj1, ...obj2 };
+console.log(obj3);
+
+const todos = [
+  { id: 1, name: 'Buy Milk' },
+  { id: 2, name: 'Pickup kids from school' },
+  { id: 3, name: 'Take out trash' },
+];
+x = Object.keys(todos[0]);
+console.log(x);
+
+x = Object.values(todos[0]);
+console.log(x);
+
+x = Object.keys(todos[0]);
+console.log(x);
+
+x = todos[0].hasOwnProperty('age');
+console.log(x);
+
+const firstName = 'John';
+const lastName = 'Doe';
+const age = 30;
+
+const person1 = {
+  firstName,
+  lastName,
+  age,
+};
+console.log(person1);
+
+// JSON
+
+const post = {
+  id: 1,
+  title: 'Post One',
+  body: 'This is a body',
+};
+
+// Convert to JSON string
+const str = JSON.stringify(post);
+
+console.log(str);
+console.log(str.length);
+
+// Parse JSON
+const obj33 = JSON.parse(str);
+console.log(obj33);
+console.log(obj33.title);
+
+const library = [
+  {
+    title: 'asdf',
+    author: 'fasdfa',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
+  {
+    title: 'hshasd',
+    author: 'asdgasd',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
+  {
+    title: 'hashasd',
+    author: 'hasdfa',
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
+];
+const { title: firstbook } = library[0];
+
+console.log(firstbook);
